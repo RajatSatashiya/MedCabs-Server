add5fec251481210d8e345c517fb74b2f3a3a250
Rajat Satashiya <kshri4398@gmail.com>
2021-11-23 18:03:32 +0530

    project completed (with room for future works)

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("what decode: " + decoded);
+    // console.log("what decode: " + decoded);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
